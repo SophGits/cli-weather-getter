28a6a5ecc99018d0d6c8f5e36e64edaeed460a35
SophGits <sophie.jiffy@gmail.com>
2015-10-18 15:57:27 +0100

    Output emoji based on London's weather today.

--- index.js
+++ index.js
@@ -5,12 +5,28 @@ var Promise = require('es6-promise').Promise,
 var APP_ID = "bd82977b86bf27fb59a04b61b657fb6f";
 
 
+// Main
+function getIcon(code) {
+  var iconMap = {
+    "01d": "🌈",
+    "02d": "☁️",
+    "03d": "☁️☁",
+    "04d": "⛅",
+    "09d": "☔",
+    "10d" : "💦",
+    "11d" : "⚡",
+    "13d": "❄",
+    "50d": "💨"
+  };
+
+  return iconMap[code];
+}
+
+
 axios.get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=" + APP_ID)
   .then(function (res) {
-    var sunset = res.data.sys.sunset;
-    sunset = new Date(sunset * 1e3);
-    console.log("Sunset is: ", sunset);
-
+    var icon = getIcon(res.data.weather[0].icon);
+    console.log(icon);
   })
   .catch(function (error) {
     console.log(error);
